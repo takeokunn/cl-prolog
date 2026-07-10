68f2753210b4cecb11b26dffb06d03bfd0a99c06
takeokunn <bararararatty@gmail.com>
2026-07-10 13:31:58 +0900

    ci: add GitHub Actions workflow and bump flake to 0.2.0
    Runs the no-ASDF core suite, the full ASDF test-op, the contract
    verifier, benchmark smoke, and example scripts on Linux and macOS,
    plus nix flake check.

--- flake.nix
+++ flake.nix
@@ -12,33 +12,62 @@
           value = f system;
         })
         systems);
+      sourceFor = pkgs: pkgs.lib.cleanSourceWith {
+        src = ./.;
+        filter = path: type:
+          (pkgs.lib.cleanSourceFilter path type)
+          && (let
+            name = builtins.baseNameOf path;
+          in
+            !(pkgs.lib.hasSuffix ".fasl" name
+              || pkgs.lib.hasSuffix ".cfasl" name
+              || pkgs.lib.hasSuffix ".dfsl" name
+              || pkgs.lib.hasSuffix ".ufasl" name
+              || pkgs.lib.hasSuffix ".core" name
+              || pkgs.lib.hasSuffix ".o" name));
+      };
     in
     {
       formatter = forAllSystems (system:
         let pkgs = import nixpkgs { inherit system; };
         in pkgs.nixpkgs-fmt);
 
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          src = sourceFor pkgs;
+        in
+        {
+          default = pkgs.sbcl.buildASDFSystem {
+            pname = "cl-prolog";
+            version = "0.2.0";
+            src = src;
+            systems = [ "cl-prolog" ];
+          };
+        });
+
       checks = forAllSystems (system:
         let
           pkgs = import nixpkgs { inherit system; };
+          src = sourceFor pkgs;
         in
         {
           default = pkgs.runCommand "cl-prolog-tests"
             {
-              nativeBuildInputs = [ pkgs.sbcl ];
+              nativeBuildInputs = [ pkgs.sbcl self.packages.${system}.default ];
             } ''
-            cp -R ${self} source
-            chmod -R u+w source
-            cd source
-            export HOME="$TMPDIR/home"
-            export XDG_CACHE_HOME="$TMPDIR/cache"
-            mkdir -p "$HOME" "$XDG_CACHE_HOME"
-            sbcl --non-interactive \
-              --eval '(require :asdf)' \
-              --eval '(asdf:load-asd (truename "cl-prolog.asd"))' \
-              --eval '(asdf:test-system :cl-prolog)'
-            touch $out
-          '';
+              cp -R ${src} source
+              chmod -R u+w source
+              cd source
+              export HOME="$TMPDIR/home"
+              export XDG_CACHE_HOME="$TMPDIR/cache"
+              mkdir -p "$HOME" "$XDG_CACHE_HOME"
+              sbcl --non-interactive \
+                --eval '(require :asdf)' \
+                --eval '(asdf:load-asd (truename "cl-prolog.asd"))' \
+                --eval '(asdf:test-system :cl-prolog)'
+              touch $out
+            '';
         });
 
       apps = forAllSystems (system:
@@ -46,7 +75,7 @@
           pkgs = import nixpkgs { inherit system; };
           test = pkgs.writeShellApplication {
             name = "cl-prolog-test";
-            runtimeInputs = [ pkgs.sbcl ];
+            runtimeInputs = [ pkgs.sbcl self.packages.${system}.default ];
             text = ''
               export XDG_CACHE_HOME="''${XDG_CACHE_HOME:-$PWD/.cache}"
               mkdir -p "$XDG_CACHE_HOME"
@@ -73,6 +102,7 @@
             packages = [
               pkgs.nixpkgs-fmt
               pkgs.sbcl
+              self.packages.${system}.default
             ];
           };
         });
