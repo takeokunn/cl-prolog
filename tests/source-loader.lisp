8b35e001faec474076cd8a582f11e0e8c998b284
takeokunn <bararararatty@gmail.com>
2026-07-13 07:31:33 +0900

    refactor: split source loader error tests

--- tests/source-loader.lisp
+++ tests/source-loader.lisp
@@ -351,40 +351,6 @@
         (make-rulebase)
         "catch(load_files([], [if(not_loaded), if(not_loaded)]), error(domain_error(load_option, _), _), true)")))
 
-(defmacro deftest-source-loading-error (name &body query-form)
-  `(deftest ,name ()
-     (let ((rulebase (make-rulebase))
-           (missing (%temporary-prolog-pathname)))
-       (is (%source-query-succeeds-p rulebase (progn ,@query-form))))))
-
-(deftest-source-loading-error source-loading-instantiation-error
-  "catch(consult(Source), error(instantiation_error, _), true)")
-
-(deftest-source-loading-error source-loading-scalar-type-error
-  "catch(consult(42), error(type_error(atom, 42), _), true)")
-
-(deftest-source-loading-error source-loading-improper-list-type-error
-  (format nil
-          "catch(load_files([~A | tail]), ~
-           error(type_error(list, [~A | tail]), _), true)"
-          (%prolog-path-atom missing)
-          (%prolog-path-atom missing)))
-
-(deftest-source-loading-error source-loading-partial-list-instantiation-error
-  (format nil
-          "catch(load_files([~A | Tail]), error(instantiation_error, _), true)"
-          (%prolog-path-atom missing)))
-
-(deftest-source-loading-error source-loading-variable-list-item-instantiation-error
-  "catch(load_files([Source]), error(instantiation_error, _), true)")
-
-(deftest-source-loading-error source-loading-missing-source-error
-  (format nil
-          "catch(consult(~A), ~
-           error(existence_error(source_sink, ~A), _), true)"
-          (%prolog-path-atom missing)
-          (%prolog-path-atom missing)))
-
 (deftest source-loader-supports-iso-declaration-directives ()
   (let ((rulebase
           (consult-prolog
